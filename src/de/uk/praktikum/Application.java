7979b272a0f9f449c0670e9f587f2fe378459adc
Dennis Demmer <Dennis.Demmer@web.de>
2020-11-17 12:16:17 +0100

    Update Application.java

--- src/de/uk/praktikum/Application.java
+++ src/de/uk/praktikum/Application.java
@@ -4,16 +4,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		
+			
 				//############################################
-		
+					
 				double preis = 1.99d;
 			
 				//wandelt den Preis in einen abgerundeten int wert um
-//				int abgerundeterPreis = 
+				int abgerundeterPreis = (int) preis;
 				
 				
 				//weist der Variablen "istAbgerundet" das Ergebnis des Vergleichs zwischen "abgerundeterPreis" und 1 zu
-//				boolean istAbgerundet = 
+				boolean istAbgerundet = abgerundeterPreis == 1;
 				
 				
 				System.out.println("Preis abgerundet: " + istAbgerundet);
@@ -22,20 +23,33 @@ public class Application {
 				
 				char c = 'A';
 				
+				
 				//wandelt den char c in seinen ASCII Wert um
-//				int ii = 
+//				int ii = c;
+				//oder
+				int ii = (int) c;
 				
 				System.out.println(ii);
+				
+				
 				
 				//############################################
 
 				
 				//Überlegt euch warum man keinen String zu einem char casten kann
 				String s = "A";
-//				char  gehtNicht = (char) s;
+			
+//				char gehtNicht = (char) s;
 				
+//				String ist ein Komplexer Datentyp und kann nicht zu einem primitiven Datentyp gecastet werden
 				
 				//############################################
+				
+				
+				//Die String Klasse hat Methoden
+				char buchstabe = s.charAt(4);
+				
+				System.out.println(buchstabe);
 
 
 	}
