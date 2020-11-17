332eadb3540b0d0338724b69da77d0df577bff93
DDemmer1 <Dennis.Demmer@web.de>
2020-11-17 08:42:12 +0100

    inital commit

---
+++ src/de/uk/praktikum/Application.java
@@ -0,0 +1,43 @@
+package de.uk.praktikum;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+				//############################################
+		
+				double preis = 1.99d;
+			
+				//wandelt den Preis in einen abgerundeten int wert um
+//				int abgerundeterPreis = 
+				
+				
+				//weist der Variablen "istAbgerundet" das Ergebnis des Vergleichs zwischen "abgerundeterPreis" und 1 zu
+//				boolean istAbgerundet = 
+				
+				
+				System.out.println("Preis abgerundet: " + istAbgerundet);
+				
+				//############################################
+				
+				char c = 'A';
+				
+				//wandelt den char c in seinen ASCII Wert um
+//				int ii = 
+				
+				System.out.println(ii);
+				
+				//############################################
+
+				
+				//Überlegt euch warum man keinen String zu einem char casten kann
+				String s = "ssda";
+//				char  gehtNicht = (char) s;
+				
+				
+				//############################################
+
+
+	}
+
+}
